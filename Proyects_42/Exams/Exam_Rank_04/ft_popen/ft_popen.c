1d31a8f091ebca2b0be0de70d411db5bb502e1d8
nicoagnz <acunagonzalez12@gmail.com>
2026-01-13 11:40:48 +0100

    .

--- Proyects_42/Exams/Exam_Rank_04/ft_popen/ft_popen.c
+++ Proyects_42/Exams/Exam_Rank_04/ft_popen/ft_popen.c
@@ -1,20 +1,21 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int ft_popen(const char *f, char *const a[], char t)
+
+int ft_popen(const char *file, char *const argv[], char type)
 {
-    int p[2];
-    if (!f || !a || (t != 'r' && t != 'w') || pipe(p) || fork())
-        ;
-    else
-    {
-        dup2(p[t == 'r'], t == 'r' ? 1 : 0);
-        close(p[0]);
-        close(p[1]);
-        execvp(f, a);
-        exit(1);
-    }
-    close(p[t == 'r']);
-    return (p[t != 'r']);
+	int pipefd[2];
+	if (!file || !argv || (type != 'r' && type != 'w') || pipe(pipefd) || fork())
+		;
+	else
+	{
+		dup2(pipefd[type == 'r'], type == 'r' ? 1 : 0);
+		close(pipefd[0]);
+		close(pipefd[1]);
+		execvp(file, argv);
+		exit(1);
+	}
+	close(pipefd[type == 'r']);
+	return (pipefd[type != 'r']);
 }
 
--- Proyects_42/Exams/Exam_Rank_04/picoshell/picoshell.c
+++ Proyects_42/Exams/Exam_Rank_04/picoshell/picoshell.c
@@ -2,36 +2,36 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 
-int picoshell(char **cmds[])
+int	picoshell(char **cmds[])
 {
-    int i = 0, fd[2], in = 0, status, ret = 0;
+	int i = 0, in = 0, ret = 0, fd[2], status;
 
-    while (cmds[i])
-    {
-        pipe(fd);
-        if (!fork())
-        {
-            dup2(in, 0);
-            if (cmds[i + 1])
-                dup2(fd[1], 1);
-            if (in)
-                close(in);
-            close(fd[0]);
-            close(fd[1]);
-            execvp(cmds[i][0], cmds[i]);
-            exit(1);
-        }
-        close(fd[1]);
-        if (in)
-            close(in);
-        in = fd[0];
-        i++;
-    }
-    if (in)
-        close(in);
-    while (wait(&status) > 0)
-        if (status)
-            ret = 1;
-    return (ret);
+	while (cmds[i])
+	{
+		pipe(fd);
+		if (!fork())
+		{
+			dup2(in, 0);
+			if (cmds[i + 1])
+				dup2(fd[1], 1);
+			if (in)
+				close(in);
+			close(fd[0]);
+			close(fd[1]);
+			execvp(cmds[i][0], cmds[i]);
+			exit(1);
+		}
+		close(fd[1]);
+		if (in)
+			close(in);
+		in = fd[0];
+		i++;
+	}
+	if (in)
+		close(in);
+	while (wait(&status) > 0)
+		if (status)
+			ret = 1;
+	return (ret);
 }
 
