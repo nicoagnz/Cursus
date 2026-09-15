ab7381af7b1dcdf53df64c9995b16c7d094580c1
nicoagnz <acunagonzalez12@gmail.com>
2026-09-15 13:22:00 +0200

    .

---
+++ Proyects_42/CPP/CPP Module 09/ex02/main.cpp
@@ -0,0 +1,13 @@
+#include "PmergeMe.hpp"
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	if (ac < 2)
+	{
+		std::cerr << "Error: No input provided." << std::endl;
+		return 1;
+	}
+
+	return 0;
+}
